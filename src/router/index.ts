53d438d0f8d57404976d84f10c6aec28b83a4832
katie oconnor <katina.surise@gmail.com>
2026-05-31 19:55:53 -0500

    GPDS-3 - fix index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,7 @@ import HomeView   from '../views/HomeView.vue'
 import TokensView from '../views/TokensView.vue'
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/',       component: HomeView },
     { path: '/tokens', component: TokensView },
